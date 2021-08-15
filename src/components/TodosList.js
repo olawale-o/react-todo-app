04f4929e7ab5d8d4e1c8073f8716e7a07b3e71bd
olawale-o <omoogunolawale@gmail.com>
2021-08-15 18:39:23 +0100

    Render todos in TodosList.js file

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -7,13 +7,26 @@ class TodosList extends React.Component {
     this.props = props;
     this.state = {};
   }
+
   render() {
+    const { todos } = this.props;
     return (
       <ul>
-
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
       </ul>
-    )
+    );
   }
 }
 
 export default TodosList;
+
+TodosList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
