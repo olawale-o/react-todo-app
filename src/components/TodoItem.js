49a991da8c68e45fa11e84b8bb5bffe3eb324094
olawale-o <omoogunolawale@gmail.com>
2021-08-15 18:52:05 +0100

    Modify TodoItem.js file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {};
+  }
+
   render() {
-    return <li>{this.props.todo.title}</li>
+    const { todo } = this.props;
+    return <li>{todo.title}</li>;
   }
 }
 
 export default TodoItem;
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
