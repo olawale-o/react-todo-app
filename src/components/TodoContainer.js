7d815b412a2481def4e0bcffbce9abf103e2efb2
olawale-o <omoogunolawale@gmail.com>
2021-08-15 18:25:51 +0100

    Add TodoContainer.js file

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <ul>
+        {todos.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+export default TodoContainer;
