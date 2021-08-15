56707c3fe948ec59279b35736c9d18199a9ec30a
olawale-o <omoogunolawale@gmail.com>
2021-08-15 18:30:31 +0100

    Add TodosList.js file

---
+++ src/components/TodosList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TodosList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {};
+  }
+  render() {
+    return (
+      <ul>
+
+      </ul>
+    )
+  }
+}
+
+export default TodosList;
