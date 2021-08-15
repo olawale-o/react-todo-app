f7897f299965b3784d56fe215d2ceaad2c377903
olawale-o <omoogunolawale@gmail.com>
2021-08-15 21:43:22 +0100

    Create handleSubmit function in InputTodo.js file

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -4,13 +4,28 @@ class InputTodo extends Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.state = {};
+    this.state = {
+      title: '',
+    };
   }
 
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    const { title } = this.state;
+    e.preventDefault();
+    console.log(title);
+  };
+
   render() {
+    const { title } = this.state;
     return (
-      <form>
-        <input type="text" placeholder="Add Todo..." />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add Todo..." value={title} onChange={this.onChange} />
         <button type="button">Submit</button>
       </form>
     );
